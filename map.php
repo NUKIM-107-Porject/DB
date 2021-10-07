aa83d245a91cf7b3ef1bb27097d259fb00623435
yuan-hub666 <61937268+yuan-hub666@users.noreply.github.com>
2021-10-07 11:56:52 +0800

    Add files via upload

---
+++ map.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MAP</title>
+</head>
+<body>
+        <input type="button" value="修改個人資料" onclick="location.href='modifypersonalinfo.php'">
+        <input type="button" value="選擇/修改所使用的信用卡種" onclick="location.href='modifypersonalcreditcard.php'">
+</body>
+</html>
